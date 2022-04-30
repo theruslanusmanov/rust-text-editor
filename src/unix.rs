aa20025482340a6f023d8d1facb8fe22f731ab2b
Ruslan Usmanov <ruslanusm6@gmail.com>
2022-04-30 18:08:50 +0500

    add fn `get_window_size`

--- src/unix.rs
+++ src/unix.rs
@@ -18,3 +18,16 @@ fn cerr(err: c_int) -> Result<(), Error> {
         _ => Err(std::io::Error::last_os_error().into()),
     }
 }
+
+/// Return the current window size as (rows, columns).
+///
+/// We use the `TIOCGWINSZ` ioctl to get window size. If it succeeds, a `Winsize` struct will be
+/// populated.
+/// This ioctl is described here: <http://man7.org/linux/man-pages/man4/tty_ioctl.4.html>
+pub fn get_window_size() -> Result<(usize, usize), Error> {
+    let mut maybe_ws = std::mem::MaybeUninit::<winsize>::uninit();
+    cerr(unsafe { libc::ioctl(STDOUT_FILENO, TIOCGWINSZ, maybe_ws.as_mut_ptr()) })
+        .map_or(None, |_| unsafe { Some(maybe_ws.assume_init()) })
+        .filter(|ws| ws.ws_col != 0 && ws.ws_row != 0)
+        .map_or(Err(Error::InvalidWindowSize), |ws| Ok((ws.ws_row as usize, ws.ws_col as usize)))
+}
